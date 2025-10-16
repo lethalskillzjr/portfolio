7b29ce7bdfe67c4567a83709c25d97c83674f8cc
Abdulhakim Aminu Ibrahim <abdulhakimaminuibrahim@gmail.com>
2025-10-16 12:27:51 +0100

    Resolved SSR error by using dynamic imports

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,21 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+import { useEffect, useState } from "react";
 // import { link } from "fs";
 // import Image from "next/image";
 // import { FaHome } from "react-icons/fa";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  });
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        {mounted && <FloatingNav navItems={navItems} />}
         <Hero />
         <Grid />
         <RecentProjects />
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
-import { title } from 'process'
-import { gridItems } from '@/data'
+"use client";
+
+import dynamic from "next/dynamic";
+import React from "react";
+import { gridItems } from "@/data";
+
+// Dynamically import only the named BentoGrid component
+const BentoGrid = dynamic(
+  () => import("./ui/BentoGrid").then((mod) => mod.BentoGrid),
+  { ssr: false }
+);
+
+// Dynamically import BentoGridItem too
+const BentoGridItem = dynamic(
+  () => import("./ui/BentoGrid").then((mod) => mod.BentoGridItem),
+  { ssr: false }
+);
 
 function Grid() {
   return (
-    <section id='about'>
-        <BentoGrid>
-            {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
-              <BentoGridItem 
-                id={id}
-                key={id}
-                title={title}
-                description={description}
-                className={className} 
-                img={img}
-                imgClassName={imgClassName}
-                titleClassName={titleClassName}
-                spareImg={spareImg}
-              />  
-            ))}
-        </BentoGrid>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
     </section>
-  )
+  );
 }
 
-export default Grid
+export default Grid;
